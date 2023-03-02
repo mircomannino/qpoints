002ed9459778efcd1af0d9f18e26e280210c8c09
Mirco Mannino <mircomannino1997@gmail.com>
2023-03-02 14:28:32 +0100

    Add install_requirements.sh

---
+++ install_requirements.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download the following github repositories:
+#   - qemu
+#   - capstone
+
+git clone https://github.com/qemu/qemu.git --depth 1
+git clone https://github.com/capstone-engine/capstone
